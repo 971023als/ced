408e9716439b079c6ffb3ebe75df0dad8dcf757c
971023als <971023als@naver.com>
2023-02-09 13:09:04 +0900

    Update U-24.sh

--- U-24.sh
+++ U-24.sh
@@ -16,17 +16,19 @@ EOF
 
 BAR
 
-# Rename the disabled NFS startup script to its original name
+# Backup NFS configuration files
+cp /etc/dfs/dfstab /etc/dfs/dfstab.bak
+cp /etc/exports /etc/exports.bak
+
+# 사용하지 않도록 설정된 NFS 시작 스크립트의 이름을 원래 이름으로 변경합니다
 if [ -f "/etc/rc.d/rc2.d/_S60nfs" ]; then
-mv /etc/rc.d/rc2.d/_S60nfs /etc/rc.d/rc2.d/S60nfs
+sudo mv /etc/rc.d/rc2.d/_S60nfs /etc/rc.d/rc2.d/S60nfs
 fi
 
-# Start the NFS related processes
-/usr/sbin/nfsd start
-/usr/sbin/statd start
-/usr/sbin/lockd start
-
-
+# NFS 관련 프로세스 시작
+/usr/sbin/nfsd restart
+/usr/sbin/statd restart
+/usr/sbin/lockd restart
 
 cat $result
 
