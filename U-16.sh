2e63209e20a7e749af113dd81b55f13a3b57807a
971023als <971023als@naver.com>
2023-02-09 12:54:41 +0900

    Update U-16.sh

--- U-16.sh
+++ U-16.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
- 
-
 . function.sh
-
- 
 
 BAR
 
@@ -24,18 +20,28 @@ TMP1=`SCRIPTNAME`.log
 
 >$TMP1  
 
+# 백업 디렉터리 정의
+backup_dir="/dev_backup"
 
-# Check if TMP1 file exists
-if [ -f "$TMP1" ]; then
-  # Restore files in /dev directory
-  while read file; do
-    sudo touch "$file"
-  done < "$TMP1"
+# 현재 /dev 디렉토리 제거
+rm -rf /dev
+
+# 제거가 성공적이었는지 확인하십시오
+if [ $? -eq 0 ]; then
+  OK "/dev 디렉토리가 제거되었습니다."
+else
+  INFO "/dev 디렉토리 제거 실패"
 fi
 
+# 백업 디렉토리를 /dev에 복사합니다
+sudo cp -R $backup_dir /dev
 
- 
-
+# 복사가 성공했는지 확인합니다
+if [ $? -eq 0 ]; then
+  OK "$backup_dir 디렉토리가 /dev에 복사되었습니다."
+else
+  WARN "$backup_dir 디렉토리 복사 실패"
+fi
  
 cat $result
 
