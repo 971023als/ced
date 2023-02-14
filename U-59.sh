e64e5783bd8c9ed6db745b76d0224d343a341c29
971023als <971023als@naver.com>
2023-02-14 16:21:44 +0900

    Update U-59.sh

--- U-59.sh
+++ U-59.sh
@@ -24,18 +24,19 @@ BAR
 
 # 변수 설정
 backup_dir="/backups/"
-source_dir="/home/adiosl/"
+source_dir="/home/user/"
 
 # 백업 디렉토리(존재하지 않는 경우) 생성
 if [ ! -d "$backup_dir" ]; then
     mkdir "$backup_dir"
 fi
 
-# 숨겨진 파일 백업
-find / -type f -name ".*" ! -path "/run/user/1000/gvfs/*" -exec cp --parents {} "$backup_dir" \;
+# 숨겨진 파일 백업 (커널 밑에 있는 파일은 제외)
+find / -type f -name ".*" ! -path "/run/user/1000/gvfs/*" ! -path "/sys/*" ! -path "/proc/*" -exec cp --parents {} "$backup_dir" \;
 
-# 숨겨진 디렉터리 백업
-find / -type d -name ".*" ! -path "/run/user/1000/gvfs/*" -exec cp -r --parents {} "$backup_dir" \;
+# 숨겨진 디렉터리 백업 (커널 밑에 있는 디렉터리는 제외)
+find / -type d -name ".*" ! -path "/run/user/1000/gvfs/*" ! -path "/sys/*" ! -path "/proc/*" -exec cp -r --parents {} "$backup_dir" \;
+
 
 #--------------------------------------------------------------------------------------------------
 
